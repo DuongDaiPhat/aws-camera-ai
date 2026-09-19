7725c2a9e295687c8f18422d69af4ada8a7f3242
Nhan123-droid <nhantran9q5@gmail.com>
2026-09-19 14:22:08 +0700

    chore(orchestrator): align local flow runtime and documentation

--- infra/minio/minio-init.sh
+++ infra/minio/minio-init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Tao bucket media + lifecycle rule xoa sau N ngay (FR-DAT-01)
+# Tao bucket media va lifecycle rule xoa sau N ngay (FR-DAT-01)
 set -e
 
 mc alias set local http://minio:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
